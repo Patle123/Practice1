03313a54294f42bd6791eb354e42051f594b05d3
Shachi Patle <shachp77@in.ibm.com>
2018-07-03 18:17:43 +0530

    first

---
+++ src/main/java/Web/Practice1/App.java
@@ -0,0 +1,13 @@
+package Web.Practice1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
